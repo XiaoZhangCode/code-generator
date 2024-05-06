c3df947530f4ab19e600ba14bdde359c1bb425d1
i1687438992 <1687438992@qq.com>
2024-05-06 21:32:23 +0800

    腾讯云

--- azhang-generator-web-frontend/src/constants/index.ts
+++ azhang-generator-web-frontend/src/constants/index.ts
@@ -11,4 +11,4 @@ export const BACKEND_HOST_PROD = 'https://azhang.online';
 /**
  * COS 访问地址
  */
-export const COS_HOST = 'https://az-1312808569.cos.ap-beijing.myqcloud.com';
+export const COS_HOST = '';
